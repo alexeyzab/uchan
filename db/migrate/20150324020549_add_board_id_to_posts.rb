3b14ea9fc44ffdb9bb432c9246c1cbb46706baf1
Alexey Zabelin <zabelin.alex@gmail.com>
2015-04-30 14:17:25 -0400

    Remove migartion

--- db/migrate/20150324020549_add_board_id_to_posts.rb
+++
@@ -1,9 +0,0 @@
-class AddBoardIdToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :board_id, :integer
-  end
-
-  def down
-    remove_column :posts, :board_id, :integer
-  end
-end
