322fedf87a3f556bded88213694b0c38fe97431d
Alexey Zabelin <zabelin.alex@gmail.com>
2015-04-30 13:28:38 -0400

    Edit the board_id migration

--- db/migrate/20150324020549_add_board_id_to_posts.rb
+++ db/migrate/20150324020549_add_board_id_to_posts.rb
@@ -2,4 +2,8 @@ class AddBoardIdToPosts < ActiveRecord::Migration
   def change
     add_column :posts, :board_id, :integer
   end
+
+  def down
+    remove_column :posts, :board_id, :integer
+  end
 end
