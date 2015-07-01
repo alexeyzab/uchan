29ebc458f9cf11366590ae4a3e0fc18ee99c366f
Alexey Zabelin <zabelin.alex@gmail.com>
2015-07-01 17:45:05 -0400

    Remove form test

--- spec/features/user_can_create_posts_spec.rb
+++ spec/features/user_can_create_posts_spec.rb
@@ -29,16 +29,6 @@ feature "user can create a post inside a topic" do
     expect(page).to have_content("Image successfully uploaded!")
   end
 
-  scenario "input gets saved if it's too long" do
-    prepare_board_topic
-
-    fill_form(:post, { post_title: "My test post", post_description: "#{'Hi' * 251}" })
-    click_on "Create Post"
-
-    expect(page).to have_content("Post description is too long")
-    expect(page).to have_content("#{'Hi' * 250}")
-  end
-
   def prepare_board_topic
     board = create(:board)
     topic = create(:topic, board: board)
