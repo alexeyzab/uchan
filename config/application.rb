1e8f4f6f46e8d76e52f0520d7763559489b4dd37
Alexey Zabelin <zabelin.alex@gmail.com>
2015-12-21 19:23:16 -0500

    Match AWS config

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,5 @@ module Uchan
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.enabled = true
   end
 end
