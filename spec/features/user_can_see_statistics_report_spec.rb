70b53da786330b4cbbe6a047c44bf1dcceac950b
Alexey Zabelin <zabelin.alex@gmail.com>
2016-01-20 13:43:26 -0500

    Edit the feature spec

--- spec/features/user_can_see_statistics_report_spec.rb
+++ spec/features/user_can_see_statistics_report_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 feature "user can see the statistics report" do
   scenario "total amount of topics" do
-    create_list(:topic, 1)
+    create(:topic)
     visit root_path
 
     expect(page).to have_content("Total Topics: 1")
