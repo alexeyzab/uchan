05973faf6cfa22acaf6bf8a1fd31db104de07e9a
Alexey Zabelin <zabelin.alex@gmail.com>
2016-03-03 15:45:02 -0500

    Update the routes for Catalog

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "boards#index"
   resources :boards, only: [:index, :show] do
+    resource :catalog, only: [:show]
     resources :topics, only: [:index, :show, :new, :create] do
       resources :posts, only: [:index, :new, :create]
     end
