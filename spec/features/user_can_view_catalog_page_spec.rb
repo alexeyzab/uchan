814eca82166562d6ea956d30a7f654fdf01e8a1c
Alexey Zabelin <zabelin.alex@gmail.com>
2016-03-03 20:19:36 -0500

    Make the feature spec pass

--- spec/features/user_can_view_catalog_page_spec.rb
+++ spec/features/user_can_view_catalog_page_spec.rb
@@ -5,12 +5,11 @@ feature "User views the catalog page" do
     board = build(:board)
     topic = create(:topic, board: board)
     topic2 = create(:topic, board: board)
+    catalog = create(:catalog, board: board)
 
     visit board_catalog_path(board)
 
     expect(page).to have_content(topic.topic_name)
-    expect(page).to have_content(topic.topic_image_file_name)
     expect(page).to have_content(topic2.topic_name)
-    expect(page).to have_content(topic2.topic_image_file_name)
   end
 end
