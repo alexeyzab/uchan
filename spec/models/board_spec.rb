b2a83fd43d07f09474ea3fa3fee84eff9bb876c5
Alexey Zabelin <zabelin.alex@gmail.com>
2016-03-16 13:21:56 -0400

    Add board spec

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -2,8 +2,16 @@ require "rails_helper"
 
 describe Board do
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:slug) }
   it { should validate_uniqueness_of(:name) }
   it { should validate_length_of(:name).
        is_at_most(40) }
   it { should have_one(:catalog) }
+  
+  describe "#to_param" do
+    it "returns the slug" do
+      board = create(:board)
+      expect(board.to_param).to eq(board.slug)
+    end
+  end
 end
