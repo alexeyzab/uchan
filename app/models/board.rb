76a790c793fede92a3d4eefd05a87ea3cebcaf90
Alexey Zabelin <zabelin.alex@gmail.com>
2016-03-16 13:22:08 -0400

    Add slug implementation

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,8 +3,13 @@ class Board < ActiveRecord::Base
   has_many :topics
   has_many :posts, through: :topics
   validates :name, presence: true, uniqueness: true, length: { maximum: 40 }
+  validates :slug, presence: true
 
   scope :interests, -> { where(category: "Interests") }
   scope :creative, -> { where(category: "Creative") }
   scope :other, -> { where(category: "Other") }
+
+  def to_param
+    slug
+  end
 end
