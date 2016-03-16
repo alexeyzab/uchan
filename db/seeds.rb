ae615fa6d199d0dec0fa32be7929c326309a189b
Alexey Zabelin <zabelin.alex@gmail.com>
2016-03-16 14:46:20 -0400

    Update seeds to include slugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 Post.destroy_all
 Topic.destroy_all
 Board.destroy_all
+Catalog.destroy_all
 
-Board.create([{ name: "Music", category: "Creative" }, { name: "Video Games", category: "Interests" }, { name:
-"Papercraft", category: "Creative" }, { name: "Travel", category: "Other"}, { name: "Fitness", category: "Other" }, { name: "Technology", category: "Interests" }])
+Board.create([{ name: "Music", slug: "music", category: "Creative" }, { name: "Video Games", slug: "video-games", category: "Interests" }, { name:
+"Papercraft", slug: "papercraft", category: "Creative" }, { name: "Travel", slug: "travel", category: "Other"}, { name: "Fitness", slug: "fitness", category: "Other" }, { name: "Technology", slug: "technology", category: "Interests" }])
 
 sample_name = ["Hi there!", "Oh, hi!", "Wow, what a great topic name", "This is
                really great", "Crazy titles all around", "Not sure what else to
