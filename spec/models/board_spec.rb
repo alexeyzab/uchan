095ceee118047bc11e6d23f1e50a14b1518d6704
Alexey Zabelin <zabelin.alex@gmail.com>
2016-03-31 18:09:43 -0400

    Fix spec

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,16 +1,37 @@
 require "rails_helper"
 
 describe Board do
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:slug) }
-  it { should validate_uniqueness_of(:name) }
-  it { should validate_length_of(:name).
-       is_at_most(40) }
-  it { should have_one(:catalog) }
-  
+  it "has proper attributes" do
+    board = build(:board, name: "Travel", slug: "travel")
+
+    expect(board).to be_valid
+  end
+
+  it "can't be created with wrong attributes" do
+    board = build(:board, name: nil, slug: nil)
+
+    expect(board).to be_invalid
+    expect(board.errors[:name]).to eq(["can't be blank"])
+    expect(board.errors[:slug]).to eq(["can't be blank"])
+  end
+
+  it "has the unique name" do
+    board = create(:board, name: "Travel")
+    board_two = build(:board, name: "Travel")
+
+    expect(board_two).to be_invalid
+  end
+
+  it "has the proper length of name" do
+    board = build(:board, name: "#{'a' * 41}")
+
+    expect(board).to be_invalid
+  end
+
   describe "#to_param" do
     it "returns the slug" do
-      board = create(:board)
+      board = create(:board, name: "Travel")
+
       expect(board.to_param).to eq(board.slug)
     end
   end
