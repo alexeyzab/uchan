37e001fd8c452e72c0fff5f06517e07a97323b64
Alexey Zabelin <zabelin.alex@gmail.com>
2016-04-01 15:15:51 -0400

    Fix strong params

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -27,7 +27,7 @@ class TopicsController < ApplicationController
   private
 
   def topic_params
-    params.require(:topic).permit(:topic_name, :description, :topic_image)
+    params.permit(:topic_name, :description, :topic_image)
   end
 
   def get_board
